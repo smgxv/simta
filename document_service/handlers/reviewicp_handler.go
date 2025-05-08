8e9165fdfcaefee2f93e456ff90f7f581cec8a36
smgxv <risqi445@gmail.com>
2025-05-08 09:38:18 +0700

    20 commit

--- document_service/handlers/reviewicp_handler.go
+++ document_service/handlers/reviewicp_handler.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"document_service/config"
-	"document_service/models"
+	"document_service/entities"
 	"encoding/json"
 	"net/http"
 
@@ -29,11 +29,46 @@ func GetICPByDosenIDHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	icpModel := models.NewICPModel(db)
-	icps, err := icpModel.GetByDosenID(dosenID)
+	query := `
+		SELECT 
+			i.id, i.user_id, i.dosen_id, i.topik_penelitian,
+			i.keterangan, i.file_path, i.status, i.created_at,
+			i.updated_at, d.nama_lengkap as dosen_nama,
+			t.nama_lengkap as nama_taruna, t.kelas
+		FROM icp i
+		LEFT JOIN dosen d ON i.dosen_id = d.id
+		LEFT JOIN taruna t ON i.user_id = t.id
+		WHERE i.dosen_id = ?
+	`
+	rows, err := db.Query(query, dosenID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+	defer rows.Close()
+
+	var icps []entities.ICP
+	for rows.Next() {
+		var icp entities.ICP
+		err := rows.Scan(
+			&icp.ID,
+			&icp.UserID,
+			&icp.DosenID,
+			&icp.TopikPenelitian,
+			&icp.Keterangan,
+			&icp.FilePath,
+			&icp.Status,
+			&icp.CreatedAt,
+			&icp.UpdatedAt,
+			&icp.DosenNama,
+			&icp.NamaTaruna,
+			&icp.Kelas,
+		)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		icps = append(icps, icp)
 	}
 
 	json.NewEncoder(w).Encode(map[string]interface{}{
