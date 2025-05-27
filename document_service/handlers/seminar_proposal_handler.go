234806436080dca8682f1f7a04f132e75ca7dbad
smgxv <risqi445@gmail.com>
2025-05-27 22:36:51 +0700

    246 commit

--- document_service/handlers/seminar_proposal_handler.go
+++ document_service/handlers/seminar_proposal_handler.go
@@ -21,7 +21,6 @@ func UploadSeminarProposalHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://104.43.89.154:8080")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
