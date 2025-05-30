d71490f8ecdcb614ed2254fffba3cc432575ebc5
smgxv <risqi445@gmail.com>
2025-05-30 15:10:32 +0700

    25 commit

--- document_service/handlers/hasil_telaah_handler.go
+++ document_service/handlers/hasil_telaah_handler.go
@@ -441,8 +441,8 @@ func GetDetailTelaahICPHandler(w http.ResponseWriter, r *http.Request) {
 func GetTarunaTopicsHandler(w http.ResponseWriter, r *http.Request) {
 	// Tangani preflight CORS
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	w.Header().Set("Content-Type", "application/json")
 
 	if r.Method == http.MethodOptions {
