7d999129722441cf9f42a5a952b7d735e4d11705
smgxv <risqi445@gmail.com>
2025-06-11 00:19:11 +0700

    107 commit

--- user_service/models/dosen.go
+++ user_service/models/dosen.go
@@ -7,13 +7,13 @@ import (
 type Dosen struct {
 	ID      string `json:"id"`
 	UserID  string `json:"user_id"`
-	Nama    string `json:"nama"`
+	Nama    string `json:"nama_lengkap"`
 	Jurusan string `json:"jurusan"`
 }
 
 func GetDosenByUserID(userID string) (Dosen, error) {
 	var dosen Dosen
-	query := `SELECT id, user_id, nama, jurusan FROM dosen WHERE user_id = $1`
+	query := `SELECT id, user_id, nama_lengkap, jurusan FROM dosen WHERE user_id = ?`
 	err := database.DB.QueryRow(query, userID).Scan(&dosen.ID, &dosen.UserID, &dosen.Nama, &dosen.Jurusan)
 	return dosen, err
 }
