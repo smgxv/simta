658c681c8ec241d912840329f240da74fa2d2305
smgxv <risqi445@gmail.com>
2025-07-15 09:00:54 +0700

    26 commit

--- .zap/scripts/jwt-login.js
+++ .zap/scripts/jwt-login.js
@@ -1,18 +1,34 @@
+// @name jwt-login
+// @scriptType authentication
+// @description Script to authenticate and retrieve JWT token
+// @engine ECMAScript : Oracle Nashorn
+
 function authenticate(helper, paramsValues, credentials) {
-    var email = credentials.getParam("email");
-    var password = credentials.getParam("password");
-  
-    var loginMsg = helper.prepareMessage();
-    loginMsg.setRequestHeader("POST /login HTTP/1.1\r\nHost: 104.43.89.154:8080\r\nContent-Type: application/json");
-    loginMsg.setRequestBody(JSON.stringify({ email: email, password: password }));
-  
-    helper.sendAndReceive(loginMsg, false);
-    var json = JSON.parse(loginMsg.getResponseBody().toString());
-  
-    if (json.token) {
-      helper.getGlobalVariables().set("token", json.token);
-    }
-  
-    return true;
+  var email = credentials.getParam("email");
+  var password = credentials.getParam("password");
+
+  var loginMsg = helper.prepareMessage();
+  loginMsg.setRequestHeader("POST /login HTTP/1.1\r\nHost: 104.43.89.154:8080\r\nContent-Type: application/json");
+  loginMsg.setRequestBody(JSON.stringify({ email: email, password: password }));
+
+  helper.sendAndReceive(loginMsg, false);
+
+  var responseBody = loginMsg.getResponseBody().toString();
+  var responseStatus = loginMsg.getResponseHeader().getStatusCode();
+
+  if (responseStatus === 200) {
+      try {
+          var json = JSON.parse(responseBody);
+          if (json.token) {
+              helper.getGlobalVariables().set("token", "Bearer " + json.token);
+              return true;
+          }
+      } catch (e) {
+          print("Login JSON parsing failed: " + e);
+      }
+  } else {
+      print("Login request failed with status: " + responseStatus);
   }
-  
+
+  return false;
+}
--- .zap/scripts/jwt-session.js
+++ .zap/scripts/jwt-session.js
@@ -1,7 +1,21 @@
-function processMessage(helper, msg) {
-    var token = helper.getGlobalVariables().get("token");
-    if (token && token.length > 0) {
-      msg.getRequestHeader().setHeader("Authorization", "Bearer " + token);
-    }
+// @name jwt-session
+// @contextType session
+// @description JWT session handler for ZAP
+// @engine ECMAScript : Oracle Nashorn
+
+function extractWebSession(httpMessage) {
+  // Ambil token dari Authorization header
+  var tokenHeader = httpMessage.getRequestHeader().getHeader("Authorization");
+  return tokenHeader;
+}
+
+function replaceWebSession(httpMessage, sessionToken) {
+  // Tambahkan token ke header jika tersedia
+  if (sessionToken != null && sessionToken.length() > 0) {
+      httpMessage.getRequestHeader().setHeader("Authorization", sessionToken);
   }
-  
+}
+
+function clearWebSessionIdentifiers() {
+  // Tidak ada data yang perlu dibersihkan
+}
