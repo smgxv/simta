51f741092466cba3299715f30b438bc8a43b948a
smgxv <risqi445@gmail.com>
2025-08-08 14:02:05 +0700

    ...

--- ta_service/entities/userentities.go
+++ ta_service/entities/userentities.go
@@ -1,12 +1,12 @@
 package entities
 
 type User struct {
-	ID          int64  `json:"id"`
-	NamaLengkap string `json:"nama_lengkap"`
-	Email       string `json:"email"`
-	Username    string `json:"username"`
-	Password    string `json:"-"` // tidak ikut dalam response JSON
-	Role        string `json:"role"`
-	Jurusan     string `json:"jurusan"`
-	Kelas       string `json:"kelas,omitempty"`
+	ID          int64   `json:"id"`
+	NamaLengkap string  `json:"nama_lengkap"`
+	Email       string  `json:"email"`
+	Username    string  `json:"username"`
+	Password    string  `json:"-"` // tidak ikut dalam response JSON
+	Role        *string `json:"role"`
+	Jurusan     *string `json:"jurusan"`
+	Kelas       *string `json:"kelas,omitempty"`
 }
